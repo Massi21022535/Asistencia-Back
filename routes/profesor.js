88e7cf7eff6d26f9ba5bca06adb1e4f8e01491e9
Leonardo Massimino <matimassimino23@gmail.com>
2025-11-04 14:46:58 -0300

    Cambio

--- routes/profesor.js
+++ routes/profesor.js
@@ -83,7 +83,6 @@ router.post(
   soloRol("profesor"),
   async (req, res) => {
     const comisionId = req.params.id;
-    const fecha = new Date();
     const { manual, contenido } = req.body;
 
     try {
@@ -105,7 +104,7 @@ router.post(
 
       const [result] = await pool.execute(
         `INSERT INTO clases (comision_id, fecha, qr_token, contenido) VALUES (?, NOW(), ?, ?)`,
-        [comisionId, fecha, qrToken, contenido || null]
+        [comisionId, qrToken, contenido || null]
       );
 
       const nuevaClaseId = result.insertId;
